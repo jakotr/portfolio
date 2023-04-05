9e866375adf45b1a8b9f2d5bb4b29d6e677a5ec3
jakotr <jakub.otreba1@gmail.com>
2023-04-05 10:51:39 +0200

    add font and some basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#22A39F', 
+        secondary: '#5D3891',
+        orange: '#FF6E31',
+        bgDark: '#18181b', //zinc-900
+        grayText: '#71717a', //zinc-500
+        textColor: '#F3EFE0',
+        test: 'bg-slate-800',
+      },
+
+    },
+    container: {
+      padding: '1rem'
+    },
   },
   plugins: [],
+  darkMode: 'class',
 }
