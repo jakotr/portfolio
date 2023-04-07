def18b76fa73e83f64ac5dc006f07b160afa3179
jakotr <jakub.otreba1@gmail.com>
2023-04-07 14:20:51 +0200

    added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-bgDark text-textColor flex justify-center py-2 text-xs'>
+      <p>
+        Jakub Otręba &copy; 2023
+      </p>
+    </div>
+  )
+}
+
+export default Footer
